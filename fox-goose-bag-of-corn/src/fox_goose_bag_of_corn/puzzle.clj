e5c54fc8e9017d469da171b6c4aaeee15ec1c9d3
Grant Maki <maki.gr@gmail.com>
2016-08-13 10:50:20 -0400

    added docstrings for fox-goose-corn

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -4,11 +4,13 @@
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
 (defn repeating-positions? [newstep plan]
+	"Determines if this step repeats a previous step"
 	(some #(= % (map set newstep)) (map #(map set %) plan)))
-
 
 ;todo: break this bad boy up
 (defn is-safe? [newstep]
+	"For a given proposed step (e.g. [[:fox :corn:] [:you :boat :goose] []]),
+	determines if the step violates any of the rules of the game"
 	(let [stepset (set (map set newstep))
 		in-the-boat (set (second newstep))]
 		(and (= (set (apply concat stepset)) #{:you :boat :fox :goose :corn})
@@ -22,13 +24,19 @@
 				true))))
 
 (defn valid-next-step? [plan newstep]
+	"ensures the given step is both safe and non-repeating"
 	(and (is-safe? newstep)
 		(not (repeating-positions? newstep plan))))
 
 (defn find-self [step]
+	"finds :you, and everyone with you.
+	e.g. (find-self [[:goose][:boat] [:you :fox :corn]]) => [:you :fox :corn]"
+
 	(first (filter #(contains? (set %) :you) step)))
 
 (defn bring-across [step passengers]
+	"adds two steps to the plan, one to put :you and your new passenger (if needed) in a boat,
+	and a second to bring :you and the passenger to the other side"
 	(let [step-minus-passengers (map (partial remove #(contains? (set passengers) %)) step)]
 		[[(first step-minus-passengers) 
 			(concat (second step-minus-passengers) passengers) 
@@ -41,14 +49,16 @@
 				(second step-minus-passengers) 
 				(last step-minus-passengers)])]))
 
-(defn who-is-with-you [step]
+(defn find-possible-passengers [step]
+	"finds all possible passengers you could take across the river.
+	e.g. (find-possible-passengers [[:goose] [:boat] [:you :fox :corn] =>
+	([:you] [:you :fox] [:you :corn])"
 	(let [stuff-to-bring (remove #(= :you %) (find-self step))]
 		(conj (map (fn[x] [:you x]) stuff-to-bring) [:you])))
 
 (defn find-next-steps[step]
-	(for [passengers (who-is-with-you step)]
+	(for [passengers (find-possible-passengers step)]
 		(bring-across step passengers)))
-
 
 (defn find-possible-moves [plan]
 	(let [last-step (last plan)
@@ -65,7 +75,6 @@
 			(map #(concat plan %) new-moves))))
 
 (defn plan-successful? [plan]
-	(println [(map set (last plan))])
 	(= (map set (last plan))
 		[#{} #{:boat} #{:you :goose :corn :fox}]))
 
@@ -73,22 +82,18 @@
 	(loop [working-plans plans
 		successful-plans []]
 		(if (empty? working-plans)
-			(do (println "working empty") successful-plans)
+			successful-plans
 			(let [this-plan (first working-plans)]
-				(println "working not empty")
 				(if (plan-successful? this-plan)
-					(do (println "found success")
-					(recur (rest working-plans) 
-						(conj successful-plans this-plan)))
-					(do (println "found fail")
+					(recur (rest working-plans)
+								 (conj successful-plans this-plan))
 					(recur (concat (rest working-plans) (generate-next-plans this-plan))
-						successful-plans)))
-				))))
-
-
+								 successful-plans))))))
 
 (defn generate-plans[]
+	"returns all successful river crossing plans given our defined starting position"
 	(generate-successful-plans[start-pos]))
 
 (defn river-crossing-plan []
+	"returns only the first successful plan"
 	(first (generate-plans)))
