820f8706414003b79e67fee34b5234d7e7ac7694
Grant Maki <maki.gr@gmail.com>
2016-09-27 08:18:06 -0400

    game of war

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -1,13 +1,32 @@
 (ns card-game-war.game)
 
-;; feel free to use these cards or use your own data structure
-(def suits [:spade :club :diamond :heart])
+(def suits [:spades :clubs :diamonds :hearts])
 (def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
 (def cards
   (for [suit suits
         rank ranks]
-    [suit rank]))
+    [rank suit]))
 
-(defn play-round [player1-card player2-card])
+(defn find-winning-card [player1-card player2-card]
+  (->> [player1-card player2-card]
+       (sort-by (juxt #(.indexOf ranks (first %))
+                      #(.indexOf suits (second %))))
+       (last)))
 
-(defn play-game [player1-cards player2-cards])
+(defn play-round [player1-cards player2-cards]
+  (let [card1 (first player1-cards) card2 (first player2-cards)]
+    (if (= card1 (find-winning-card card1 card2))
+      [(concat (rest player1-cards) [card1 card2]) (rest player2-cards)]
+      [(rest player1-cards) (concat (rest player2-cards) [card2 card1])])))
+
+(defn play-game [player1-cards player2-cards]
+  (loop [[player1-cards player2-cards] (play-round player1-cards player2-cards)]
+    (if (= 0 (count player2-cards))
+      :player1
+      (if (= 0 (count player1-cards))
+        :player2
+        (recur (play-round player1-cards player2-cards))))))
+
+(defn war! []
+  (let [stacks (partition 26 (shuffle cards))]
+    (play-game (first stacks) (second stacks))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -4,16 +4,32 @@
 
 
 ;; fill in  tests for your game
+(deftest test-find-winning-card
+  (testing "queens are higher rank than jacks"
+    (is (= [:queen :spades] (find-winning-card [:queen :spades] [:jack :spades]))))
+  (testing "kings are higher rank than queens"
+    (is (= [:king :diamonds] (find-winning-card [:king :diamonds] [:queen :diamonds]))))
+  (testing "aces are higher rank than kings"
+    (is (= [:ace :spades] (find-winning-card [:king :diamonds] [:ace :spades]))))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= [:ace :clubs] (find-winning-card [:ace :clubs] [:ace :spades]))))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= [5 :diamonds] (find-winning-card [5 :diamonds] [5 :clubs]))))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= [2 :hearts] (find-winning-card [2 :hearts] [2 :diamonds])))))
+
 (deftest test-play-round
-  (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+  (testing "player 1 should win our test setup and take player 2's card"
+    (is (= (play-round [[:king :spades] [2 :hearts]] [[10 :clubs] [5 :diamonds]])
+           [[[2 :hearts] [:king :spades] [10 :clubs]] [[5 :diamonds]]])))
+  (testing "player 2 should win our test setup and take player 2's card"
+    (is (= (play-round [[:king :spades] [2 :hearts]] [[:king :hearts] [5 :diamonds]])
+           [[[2 :hearts]] [[5 :diamonds] [:king :hearts] [:king :spades]]]))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
-
+  (testing "player2 loses when they run out of cards"
+    (is (= :player1 (play-game [[:king :hearts] [:ace :spades]]
+                               [[10 :diamonds] [2 :clubs]]))))
+  (testing "player1 loses when they run out of cards"
+      (is (= :player2 (play-game [[:king :hearts] [:ace :spades]]
+                                 [[:ace :diamonds] [2 :clubs]])))))
