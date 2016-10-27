44b3f0d558469638cb3dbf1d29eae24c14622ffc
Grant Maki <maki.gr@gmail.com>
2016-10-27 07:54:42 -0400

    updates for tiny maze

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -5,23 +5,39 @@
               [1  0 1]
               [1  0 :E]])
 
+(def example2 [[:S 0 0 1]
+               [1  1 0 0]
+               [1  0  0 1]
+               [1  1  0 :E]])
+
+(def example3 [[:S 0 0 0]
+               [1  0 1 0]
+               [0  0 1 0]
+               [0  0 0 :E]])
+
 (defn maze-dimensions [maze]
+  "Returns the dimensions of the maze"
   [(count maze) (count (first maze))])
 
 (defn all-possible-neighbors [[x y]]
+  "gets all of the neighbors of a pos"
   [[(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)]])
 
 (defn within-maze? [maze loc]
+  "determines whether a location is inside the maze"
   (every? true? (map > (maze-dimensions maze) loc)))
 
 (defn valid-step? [maze loc]
+  "determines of a given location is a valid place to go for a given maze"
   (and (within-maze? maze loc)
        (contains? #{:E 0} (get-in maze loc))))
 
 (defn possible-next-steps [maze loc]
+  "given a maze and loc, find all neighboring locations that are valid steps"
   (filter #(valid-step? maze %) (all-possible-neighbors loc)))
 
 (defn finished? [maze]
+  "determine if the maze is complete (meaning, :E has been marked :x)"
   (-> maze
       (flatten)
       (set)
@@ -29,20 +45,25 @@
       (not)))
 
 (defn make-step [maze loc]
+  "marks a location with :x"
   (update-in maze loc (constantly :x)))
 
 (defn start-loc [maze]
+  "find the :S location"
   (let [start-row (first (filter #(contains? (set %) :S) maze))]
     [(.indexOf start-row :S) (.indexOf maze start-row)]))
 
 (defn walk-maze [maze loc]
+  "returns an array of all the possible ways to solve the maze"
   (if (finished? maze)
-    maze
+    [maze]
     (let [next-steps (possible-next-steps maze loc)]
-      (first (->> next-steps
-           (map #(make-step maze %))
-           (map #(walk-maze %2 %1) next-steps))))))
+        (->> next-steps
+             (map #(make-step maze %))
+             (mapcat #(walk-maze %2 %1) next-steps))
+        )))
 
 
 (defn solve-maze [maze]
-  (walk-maze (make-step maze (start-loc maze)) (start-loc maze)))
+  "takes the first solution returned by walk-maze"
+  (first (walk-maze (make-step maze (start-loc maze)) (start-loc maze))))
